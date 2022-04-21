2789cf4d9ddc01be0518774111513ded003825fc
ale-gen <o.generowicz@gmail.com>
2022-04-21 13:30:40 +0200

    Add polygon types

--- Polygon.py
+++ Polygon.py
@@ -1,3 +1,15 @@
+from PolygonType import PolygonType
+
+
 class Polygon:
     def __init__(self, vertices):
         self.vertices = vertices
+        self.perimeter = self.__count_perimeter()
+        self.type = self.__get_polygon_type()
+
+    def __count_perimeter(self):
+        return 0
+
+    def __get_polygon_type(self):
+        return PolygonType.convex
+
---
+++ PolygonType.py
@@ -0,0 +1,6 @@
+import enum
+
+
+class PolygonType(enum.Enum):
+    convex = "Convex"
+    concave = "Concave"
